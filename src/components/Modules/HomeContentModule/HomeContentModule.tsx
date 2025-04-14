1f81fd04ab6baab9e189084e4165e3ce5ca9d010
verd <37105170+ellsurge@users.noreply.github.com>
2025-04-14 13:49:37 +0000

    feat: Add User model and migration, update HomeContentModule layout, and create SkeletonAtom component

--- src/components/Modules/HomeContentModule/HomeContentModule.tsx
+++ src/components/Modules/HomeContentModule/HomeContentModule.tsx
@@ -3,5 +3,9 @@ type Props = {
 };
 
 export const HomeContentModule = (props: Props) => {
-  return <div>{props.title}</div>;
+  return <div className="flex flex-col items-center justify-center w-full h-full gap-4 pt-[40px]">
+    <div className="w-[400px] h-[400px] bg-gray-400 rounded-2xl ">
+
+    </div>
+  </div>;
 };
